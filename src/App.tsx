6148306b8dad46756563e5e5a10a05df5d9658c7
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2026-02-06 16:22:27 +0100

    Removed unused function definition in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { analyzeImageMetadata, selectSourceFolder } from './lib/vision';
 import "./App.css";
 import PicksyView from "./PicksyView";
 
 function App() {
-  async function test() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    const result = await selectSourceFolder();
-    if (result && result.length > 0) {
-      // Example: analyze metadata of the first image
-      const metadata = await Promise.all(result.map(r => analyzeImageMetadata(r)));
-      console.log(metadata);
-    }
-  }
-
   return (
     <main className="container">
       <PicksyView/>
