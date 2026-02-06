287decb14657503da0b9f0ba9b14ce488af12fc2
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2026-02-06 14:21:20 +0100

    Add image selection functionality and implement image metadata analysis

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { selectSourceFolder } from "./lib/vision";
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
-  async function greet() {
+  async function test() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("select_source_folder", { name }));
+    const result = await selectSourceFolder();
+    if (result) {
+      setGreetMsg(`Selected ${result.path} with ${result.image_count} images`);
+    }
   }
 
   return (
@@ -33,15 +35,10 @@ function App() {
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          test();
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
+        <button type="submit">Select Library</button>
       </form>
       <p>{greetMsg}</p>
     </main>
