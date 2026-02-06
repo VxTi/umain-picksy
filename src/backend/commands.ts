b27abd9ed7dd609a4a55b3ab7ccd48f0fa37589d
Artem Nesterenko <artem.nesterenko@umain.com>
2026-02-06 16:08:13 +0100

    format commands

--- src/backend/commands.ts
+++ src/backend/commands.ts
@@ -6,8 +6,11 @@ type CommandEntry = {
   result: Schema.Schema<any, any, any>;
 };
 
-const PhotoSchema = Schema.Struct({ path: Schema.String, content: Schema.Uint8ArrayFromBase64 });
-const OpenFolderArgsSchema = Schema.Struct({folder: Schema.String});
+const PhotoSchema = Schema.Struct({
+  path: Schema.String,
+  content: Schema.Uint8ArrayFromBase64,
+});
+const OpenFolderArgsSchema = Schema.Struct({ folder: Schema.String });
 const OpenFolderResultSchema = Schema.Struct({
   photos: Schema.Array(PhotoSchema),
 });
@@ -20,5 +23,9 @@ export const CommandSchemas = {
 } as const satisfies Record<string, CommandEntry>;
 
 export type Command = keyof typeof CommandSchemas;
-export type Args<C extends Command> = Schema.Schema.Type<(typeof CommandSchemas)[C]["args"]>;
-export type Result<C extends Command> = Schema.Schema.Type<(typeof CommandSchemas)[C]["result"]>;
+export type Args<C extends Command> = Schema.Schema.Type<
+  (typeof CommandSchemas)[C]["args"]
+>;
+export type Result<C extends Command> = Schema.Schema.Type<
+  (typeof CommandSchemas)[C]["result"]
+>;
