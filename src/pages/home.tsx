4a0c21103959dcc05945cbe2b819f1c3a4789987
Senad Bajramovic <senad.bajramovic@punchkickinteracvie.com>
2026-02-07 10:19:33 +0100

    Updated homepage logo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,6 @@ import PicksyView from "../PicksyView";
 export default function Home() {
 	return (
 		<main className="h-screen bg-background overflow-hidden">
-
 			<div className="fixed top-4 right-4 z-50">
 				<ThemeToggle />
 			</div>
@@ -12,3 +11,4 @@ export default function Home() {
 		</main>
 	);
 }
+xw
